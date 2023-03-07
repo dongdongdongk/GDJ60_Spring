2d9d8598f900cf1637f513b9d100d93ffc9031cf
dongdongdongk <dhk9309@gmail.com>
2023-03-07 09:04:32 +0900

    test

--- src/main/webapp/WEB-INF/views/bankBook/detail.jsp
+++ src/main/webapp/WEB-INF/views/bankBook/detail.jsp
@@ -48,23 +48,28 @@
 
 	</div>
 
-	<div class="modal" tabindex="-1">
+	<!--  Update Form Modal -->
+	<!-- Modal -->
+	<div class="modal fade" id="contentsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 		<div class="modal-dialog">
-		  <div class="modal-content">
+		<div class="modal-content">
 			<div class="modal-header">
-			  <h5 class="modal-title">Modal title</h5>
-			  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			<h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
 			<div class="modal-body">
-			  <p>Modal body text goes here.</p>
+				<div class="form-floating">
+					<textarea class="form-control" placeholder="Leave a comment here" id="contents"></textarea>
+					<label for="contents">Comments</label>
+				</div>
 			</div>
 			<div class="modal-footer">
-			  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-			  <button type="button" class="btn btn-primary">Save changes</button>
+			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="closeModal">Cancel</button>
+			<button type="button" class="btn btn-primary" data-comment-num="" id="contentsConfirm">확인</button>
 			</div>
-		  </div>
 		</div>
-	  </div>
+		</div>
+	</div>
 
 	
 
--- src/main/webapp/resources/js/bankBookReply.js
+++ src/main/webapp/resources/js/bankBookReply.js
@@ -1,132 +1,146 @@
 const replyAdd = document.getElementById("replyAdd");
 const replyContents = document.getElementById("replyContents");
 const commentListResult = document.getElementById("commentListResult");
+//const pageLink = document.querySelectorAll(".page-link");
 
-replyAdd.addEventListener("click",function(){
+const contentsConfirm = document.getElementById('contentsConfirm');
+const closeModal = document.getElementById('closeModal');
+
+
+replyAdd.addEventListener("click", function(){
+
     let xhttp = new XMLHttpRequest();
 
-    //URL, Method 정보
-    xhttp.open("POST","../bankBookComment/add");
- 
-    //header 정보
+    xhttp.open("POST", "../bankBookComment/add");
+
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-    //요청 발생 POST일 경우 parameter 전송
     xhttp.send("contents="+replyContents.value+"&bookNum="+replyAdd.getAttribute('data-book-bookNum'));
- 
-    //응답 처리
-    xhttp.addEventListener("readystatechange", function(){
-        if(this.readyState==4 && this.status==200){
+
+    xhttp.addEventListener('readystatechange', function(){
+        if(this.readyState==4&&this.status==200){
             if(this.responseText.trim()==1){
-                alert('댓글이 등록 되었습니다.')
+                alert('댓글이 등록 되었습니다')
                 replyContents.value="";
                 getList();
             }else {
-                alert('댓글 등록에 실패하였습니다.')
+                alert('댓글 등록에 실패 했습니다')
             }
+
         }
-     })
+    })
+
 })
 
-//bookNum으로 댓글목록
 getList(1);
+
 function getList(page){
 
-    let count = 0;
+    let count=0;
 
     let xhttp = new XMLHttpRequest();
 
-    xhttp.open("GET", "/bankBookComment/list?bookNum="+replyAdd.getAttribute('data-book-bookNum')+"&page="+page)
+    xhttp.open("GET", "/bankBookComment/list?bookNum="+replyAdd.getAttribute('data-book-bookNum')+"&page="+page);
 
-    xhttp.send();
-
+    
     xhttp.addEventListener("readystatechange", function(){
-        if(this.readyState==4 && this.status==200){
-            commentListResult.innerHTML = this.responseText.trim();
-            count ++;
-        }
+        if(this.readyState==4&&this.status==200){
+            commentListResult.innerHTML=this.responseText.trim();
+            count++;
+        }        
     })
     
-    //0이 출력 : 비동기 방식이기 때문에
-    console.log("count :"+count);
+    xhttp.send();
+
+    //0이 출력 : 비동기 방식이기 때문
+    console.log("count : ", count);
+
 }
 
-//page요청할 수 있도록 만든 이벤트
-commentListResult.addEventListener("click",function(e){
+//page
+commentListResult.addEventListener("click", function(e){
     let pageLink = e.target;
     if(pageLink.classList.contains("page-link")){
-        let page = pageLink.getAttribute('data-board-page');
+        let page = pageLink.getAttribute("data-board-page");
         getList(page);
     }
 
     e.preventDefault();
+    
 
-})
-
+});
 
 //delete
-
-commentListResult.addEventListener("click",function(e){
-    let del= e.target;
+commentListResult.addEventListener("click", function(e){
+    let del = e.target;
     if(del.classList.contains("del")){
         let xhttp = new XMLHttpRequest();
         xhttp.open("POST", "../bankBookComment/delete");
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.send("num="+del.getAttribute("data-comment-num"));
-
-        xhttp.addEventListener("readystatechange",function(){
-            if(this.readyState==4&&this.status==200) {
-                let result  = this.responseText.trim();
-                if(result>0) {
-                    alert("댓글이 삭제 되었습니다.")
+        xhttp.addEventListener("readystatechange", function(){
+            if(this.readyState==4&&this.status==200){
+                let result = this.responseText.trim();
+                if(result>0){
+                    alert('댓글이 삭제 되었습니다');
                     getList(1);
                 }else {
-                    alert("삭제 실패");
+                    alert('삭제 실패');
                 }
-            }
+                
+            }        
         })
-
     }
 
-})
+    e.preventDefault();
+    
+});
 
 //update
-
-commentListResult.addEventListener("click",function(e){
+commentListResult.addEventListener("click", function(e){
     let updateButton = e.target;
     if(updateButton.classList.contains("update")){
-       let num = updateButton.getAttribute("data-comment-num");
-       let contents = document.getElementById("contents" + num);
-       contents.firstChild.removeAttribute("readonly");
-       let btn = document.createElement("button");
-       let attr = document.createAttribute("class");
-       attr.value = "brn brn-primary";
-       btn.setAttribute(attr);
-       contents.appendChild(btn);
-       attr = document.createTextNode("확인");
-       btn.appendChild(attr);
-       
-       btn.addEventListener("click", function(){
-            let xhttp = new XMLHttpRequest();
-            xhttp.open("POST", "../bankBookComment/update");
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.send("num="+num+"&contents="+contents.firstChild.value);
-            xhttp.addEventListener("readystatechange",function(){
-                if(this.readyState==4&&this.status==200) {
-                    let result  = this.responseText.trim();
-                    if(result>0) {
-                        alert("수정 성공.")
-                        getList(1);
-                    }else {
-                        alert("수정 실패");
-                    }
-                }
-            })
-
-       })
-       
-       
-
+        
+        let num = updateButton.getAttribute("data-comment-num");
+        let contents = document.getElementById("contents"+num); //td
+        console.log(contents);
+        let contentsTextArea=document.getElementById("contents")//Modal textarea
+        console.log(contentsTextArea);
+        //value
+        contentsTextArea.value=contents.innerText;
+        contentsConfirm.setAttribute("data-comment-num", num);
     }
+    
+    e.preventDefault();
+    
+});
 
+
+//
+contentsConfirm.addEventListener("click", function(){
+    console.log("Update Post");
+    let updateContents = document.getElementById("contents").value;
+    let num = contentsConfirm.getAttribute("data-comment-num");
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "../bankBookComment/update");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("num="+num+"&contents="+updateContents);
+    xhttp.addEventListener("readystatechange", function(){
+        if(this.readyState==4&&this.status==200){
+            let result = this.responseText.trim();
+            if(result>0){
+                alert('수정 성공');
+                closeModal.click();
+                getList(1);
+            }else {
+                alert('삭제 실패');
+            }
+            
+        }        
+    });
+    
 })
+
+
+
